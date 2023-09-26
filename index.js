c998ade3c0736de2e17809a9da1387e9174fb1fd
storm <stormarasa2@gmail.com>
2023-09-26 12:55:43 +0300

    assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solutions in this file
+
+function writeCards (names, occasion){
+    const thankYou= [];
+    for (let i=0; i<names.length; i++){
+        const message = `Thank You, ${names[i]}, for the wonderful surprise gift! `
+        thankYou.push(message);
+
+    }
+    return thankYou;
+}
